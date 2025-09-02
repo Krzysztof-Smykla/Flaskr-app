f62fe84e36e3f45e0e090df5ed7a1ec9066b697f
Krzysztof <Krzysztof.smykla.agh@gmail.com>
2025-09-02 13:06:45 +0200

    Created base html file with Jinja python templates in the flaskr/templates directory.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -77,6 +77,13 @@ def logout():
     session.clear()
     return render_template(url_for('index'))
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
 
+        return view(**kwargs)
 
+    return wrapped_view
 
